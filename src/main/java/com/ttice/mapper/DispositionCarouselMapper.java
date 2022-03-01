0fbfe60658472bff8e47bb059bb204f21d4116f3
Thecosy <23339097@qq.com>
2022-03-01 21:56:15 +0800

    备注信息

---
+++ src/main/java/com/ttice/mapper/DispositionCarouselMapper.java
@@ -0,0 +1,25 @@
+package com.ttice.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ttice.entity.Article;
+import com.ttice.entity.DispositionCarousel;
+import com.ttice.entity.User;
+import com.ttice.vo.ArticleVO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author admin
+ * @since 2022-01-13
+ */
+@Repository
+public interface DispositionCarouselMapper extends BaseMapper<DispositionCarousel> {
+    @Select("select * from disposition_carousel  order by id")
+    List<DispositionCarousel> selectAll();
+}
