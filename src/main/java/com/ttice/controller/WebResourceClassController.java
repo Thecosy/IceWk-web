e5ae1d37a4a217b831e04c28cf26eb76c2f1e1ed
NgShow <87817554+Thecosy@users.noreply.github.com>
2022-04-24 10:40:33 +0800

    获取资源分类列表

--- src/main/java/com/ttice/controller/WebResourceClassController.java
+++ src/main/java/com/ttice/controller/WebResourceClassController.java
@@ -1,9 +1,13 @@
 package com.ttice.controller;
 
 
+import com.ttice.entity.ResourceClass;
+import com.ttice.mapper.ResourceClassMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,6 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/WebResourceClass")
 public class WebResourceClassController {
+
+    @Autowired
+    private ResourceClassMapper resourceClassMapper;
+
+    @ApiOperation(value = "获取全部资源分类列表")
+    @GetMapping("/getResourceClasslist")
+    public List<ResourceClass> getResourceClasslist(
+    ){
+        return resourceClassMapper.selectList(null);
+    }
 
 }
 
